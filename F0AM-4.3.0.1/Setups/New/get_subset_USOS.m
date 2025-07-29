4e9e505a2ac5411cf718551d2866bc00adc9ac60
vanessa mac <vanessa.sun@utah.edu>
2025-07-28 19:52:28 -0600

    ran some new setups

--- F0AM-4.3.0.1/Setups/New/get_subset_USOS.m
+++ F0AM-4.3.0.1/Setups/New/get_subset_USOS.m
@@ -3,7 +3,7 @@ function [out, sun]= get_subset_USOS(t_start, t_end)
     
     % Load in MATLAB version of the filled merge of campaign's data! 
     dirpath2USOS='/Users/vanessasun/Documents/phd/utah/research/USOS_shared/F0AM-4.3.0.1/Campaign_Data/matlab_merge/parked/corrected/';
-    USOSfilename = '20240804_20240808_30min_CSL_mobile_lab_parked_with_interp_struct_for_MATLAB.mat';
+    USOSfilename = '20240804_20240808_30min_CSL_mobile_lab_parked_with_interp_pan_interp_struct_for_MATLAB.mat';
     fullpath2USOS = fullfile(dirpath2USOS,USOSfilename);
     load(fullpath2USOS); % loads struct named USOS. 
     
