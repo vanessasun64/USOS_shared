d378c6483d9138c6c7028a8d5196107a7d25bec7
vanessa_chpc <vanessa.sun@utah.edu>
2025-09-30 21:45:45 -0600

    updating a few stubborn files

---
+++ global_scripts/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sept. 25, 2025
+@author: Vanessa Sun
+Email: vanessa.sun@utah.edu
+GitHub: @vanessasun64
+"""
+
+__name__='dirpath'
+__all__=['dirpath']
+
+import dirpath
