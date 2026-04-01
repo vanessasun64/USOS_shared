4093dabb31740e3f3489026206d4e76c841c62ff
vanessa_chpc <vanessa.sun@utah.edu>
2026-04-01 17:20:44 -0600

    Updated dictionaries for USOS data to include day types such as weekdays, weekends, day of week. Also modified mda8 ozone functions.

--- F0AM_run_analysis/usos_dictionaries.py
+++ F0AM_run_analysis/usos_dictionaries.py
@@ -7,10 +7,14 @@ dirpath = '/uufs/chpc.utah.edu/common/home/haskins-group1/users/vsun/USOS_shared
 interp_vals_filepath = '/uufs/chpc.utah.edu/common/home/haskins-group1/users/vsun/USOS_shared/CampaignData_and_Merges/R0/CSL_MobileLab_Parked/F0AM_filled/interpolated_values_15min.csv'
 df_interp_vals = pd.read_csv(interp_vals_filepath, index_col = 'time_local', parse_dates = True)
 
-whole_campaign_dict = {}
-voc_available_days_dict = {}
+# whole_campaign_dict = {}
+# voc_available_days_dict = {}
 
-def mda8_calculate(date_limit, save_filename):
+# def voc_available_days_slicing(whole_campaign_input, varname_for_dict_saving):
+#     voc_available_days_dict[varname_for_dict_saving]= whole_campaign_input[(whole_campaign_input >= '2024-07-19') & (whole_campaign_input <= '2024-08-12')]
+#     return voc_available_days_dict
+
+def mda8_calculate(o3_8hr_rolling_avg_save_filename):
     df_o3_obs = df_interp_vals['O3_ppbv']
     #resample from 15 minute intervals to hourly
     mda8_data = df_o3_obs.resample('1h').mean()
@@ -22,7 +26,7 @@ def mda8_calculate(date_limit, save_filename):
     #print("mda8_data['8hr_rolling_avg_O3_obs']: ", mda8_data['8hr_rolling_avg_O3_obs'])
     df_rolling_8hr_avg_ozone = mda8_data
     df_rolling_8hr_avg_ozone.index.name = 'time_local'
-    savepath = dirpath + 'F0AM_run_analysis/' + save_filename + '.csv'
+    savepath = dirpath + 'F0AM_run_analysis/' + o3_8hr_rolling_avg_save_filename + '.csv'
     df_rolling_8hr_avg_ozone.to_csv(savepath)
     print('Saved to:' + savepath)
 
@@ -36,15 +40,37 @@ def mda8_calculate(date_limit, save_filename):
     # Select daytime values only where MD8A > 70
     exceedance_name = 'Exceedance_day_' + 'O3'
     exceedance_day_ozone = daily_max_8hr_avg_ozone[daily_max_8hr_avg_ozone >= 70].index.floor('D')
-
-    whole_campaign_dict['Exceedance_days'] = exceedance_day_ozone
-    voc_available_days_dict['Exceedance_days']= exceedance_day_ozone[(exceedance_day_ozone >= '2024-07-19') & (exceedance_day_ozone <= '2024-08-12')]
-
-    return whole_campaign_dict['Exceedance_days'], voc_available_days_dict['Exceedance_days']
-
-# def weekdays_and_weekends():
-
+    
+    return exceedance_day_ozone
+def weekdays_weekends_dayofweek():
+    df_weekdays = df_interp_vals.index[df_interp_vals.index.dayofweek < 5]
+    df_weekends = df_interp_vals.index[df_interp_vals.index.dayofweek >= 5]
+    df_mondays = df_interp_vals.index[df_interp_vals.index.dayofweek == 0]
+    df_tuesdays = df_interp_vals.index[df_interp_vals.index.dayofweek == 1]
+    df_wednesdays = df_interp_vals.index[df_interp_vals.index.dayofweek == 2]
+    df_thursdays = df_interp_vals.index[df_interp_vals.index.dayofweek == 3]
+    df_fridays = df_interp_vals.index[df_interp_vals.index.dayofweek == 4]
+    df_saturdays = df_interp_vals.index[df_interp_vals.index.dayofweek == 5]
+    df_sundays = df_interp_vals.index[df_interp_vals.index.dayofweek == 6]
+    return df_weekdays, df_weekends, df_mondays, df_tuesdays, df_wednesdays, df_thursdays, df_fridays, df_saturdays, df_sundays
 # def smokedays_and_smokefreedays():
 
-whole_campaign_dict['Exceedance_days'], voc_available_days_dict['Exceedance_days'] = mda8_calculate(date_limit = None, save_filename = 'full_campaign_rolling_8hr_avg_ozone')
-print(voc_available_days_dict['Exceedance_days'])
+df_weekdays, df_weekends, df_mondays, df_tuesdays, df_wednesdays, df_thursdays, df_fridays, df_saturdays, df_sundays = weekdays_weekends_dayofweek()
+
+whole_campaign_dict = {
+    'Exceedance_days': mda8_calculate(save_filename = 'full_campaign_rolling_8hr_avg_ozone'),
+    'Weekdays': df_weekdays,
+    'Weekends': df_weekends,
+    'Mondays': df_mondays, 
+    'Tuesdays': df_tuesdays, 
+    'Wednesdays': df_wednesdays, 
+    'Thursdays': df_thursdays, 
+    'Fridays': df_fridays, 
+    'Saturdays': df_saturdays, 
+    'Sundays': df_sundays
+    }
+
+
+# voc_available_days_dict['Exceedance_days'] = voc_available_days_slicing(whole_campaign_input = whole_campaign_dict['Exceedance_days'], 
+#                                                     varname_for_dict_saving = 'Exceedance_days')
+
